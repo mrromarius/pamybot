f2fbe4767f0b0a3b7929689b8ea5a44e30eebaad
mrromarius <romashev.al.s@gmail.com>
2022-08-17 17:07:30 +0500

    Сделали выдергивание из потока аудио файл и отправку пользователю

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 # TO-DO: Вынести из бота русскоязыную общалку в отдельный ресурсный файл
+# TO-D0: Поработать над форматом сообщений, добавить красивостей
+import os
+import re
+
 import telebot
-from yt_dlp import YoutubeDL
+from pytube import YouTube
 
 import config  #config for bot?
 
@@ -9,16 +13,26 @@ bot=telebot.TeleBot(input("Введите TOKEN бота для запуска: 
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
     # given message from user and load content
-    if message.text.find("https://you") != -1:
-        bot.send_message(message.chat.id, "Приступаю к работе! U+1F916")
-        bot.send_video(message.chat.id, get_audio_file(message.text))
+    if message.text.find("https://") != -1:
+        bot.send_message(message.chat.id, "Приступаю к работе! \U0001F916")
+        file_name = get_audio_file(message.text)
+        bot.send_audio(message.chat.id, open(file_name, 'rb'))
+        os.remove(file_name)
+        
     elif message.text in ['Привет', 'Hello', 'Hi', 'Здравствуйте']:
-        bot.send_message(message.chat.id, "Здравствуй, юный падаван! U+1F608\nГотов помочь тебе с подкастами или музыкой\nПришли ссылку с Youtube")
+        bot.send_message(message.chat.id, "Здравствуй! \U0001F601\nПревращаю видео в подкасты или музыку\nПришли ссылку с Youtube")
     else:
-        bot.send_message(message.chat.id, "Моя твоя не понимать :(")
+        bot.send_message(message.chat.id, "Моя твоя не понимать \U0001F616")
 
 def get_audio_file(link):
     # convert video in audio
-    with YoutubeDL(config.ydl_opts) as ydl:
-        return ydl.download([link])
+    try:
+        yt_obj = YouTube(link)
+        filters = yt_obj.streams.filter(abr="128kbps", progressive=False, type="audio")
+        file_name = re.sub('\W+', ' ',yt_obj.title) + '.mp4'
+        filters.get_audio_only().download('audio', filename=file_name)
+        return 'audio/' + file_name
+    except Exception as e:
+        print(e)
 bot.polling(non_stop=True)
+
