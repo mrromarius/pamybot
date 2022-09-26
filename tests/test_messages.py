cc03b66accd53db06824e72a4b597035d0e8b718
Alexander Romashev <romashev.al.s@gmail.com>
2022-09-26 06:42:56 +0500

    test processed

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -6,7 +6,7 @@ import pytest_asyncio
 import logging
 
 from bot import command_start, command_echo
-from .types.dataset import MESSAGE_START, MESSAGE
+from .types.dataset import MESSAGE_START, MESSAGE, CHAT
 
 
 # @pytest.skip
@@ -17,8 +17,9 @@ async def test_reply_commands_start_and_help():
                 + "Пришли ссылку с [Youtube](https://www.youtube.com/) и получи обратно аудиофайл который можно слушать где удобно"
 
     message_mock = AsyncMock(MESSAGE_START)
+    message_mock.chat = CHAT
     logging.warning("Тестинфо")
-    logging.warning(message_mock["message_id"])
+    logging.warning(message_mock.chat)
     await command_start(message=message_mock)
-    message_mock.answer.assert_called_with(text_reply)
+    message_mock.send_message.assert_called_with(text_reply)
 
